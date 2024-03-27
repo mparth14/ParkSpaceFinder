071c556bd07ce784abd0d47aa264ff29844cebd7
Dheemanth Rajendra Prasad Kumawat <dh243394@dal.ca>
2024-03-27 17:51:02 -0300

    added book now button

--- app/src/main/java/com/parkspace/finder/ui/parkingDetail/ParkingDetailScreen.kt
+++ app/src/main/java/com/parkspace/finder/ui/parkingDetail/ParkingDetailScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -325,6 +326,15 @@ fun ParkingDetailScreen(
                         modifier = Modifier.padding(vertical = 8.dp)
                     )
                     AddressBar(context = LocalContext.current, parkingSpace = space)
+                    Button(
+                        onClick = { navController.navigate("ROUTE_BOOKINGS") },
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .fillMaxWidth(),
+                        //colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.primary)
+                    ) {
+                        Text(text = "Book Now")
+                    }
                 }
             }
         }
