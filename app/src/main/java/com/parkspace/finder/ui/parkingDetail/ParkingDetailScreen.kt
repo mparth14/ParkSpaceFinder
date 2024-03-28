37f1c3e8ed9553a884cc42629ccf2a2f7c841434
Dheemanth Rajendra Prasad Kumawat <dh243394@dal.ca>
2024-03-28 16:30:31 -0300

    fixed scrollbar issue

--- app/src/main/java/com/parkspace/finder/ui/parkingDetail/ParkingDetailScreen.kt
+++ app/src/main/java/com/parkspace/finder/ui/parkingDetail/ParkingDetailScreen.kt
@@ -16,6 +16,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
@@ -152,6 +154,7 @@ fun ParkingDetailScreen(
 //            }
         }
     }
+    val scrollState = rememberScrollState()
     Scaffold(
         topBar = {
             TopAppBar(
@@ -187,7 +190,8 @@ fun ParkingDetailScreen(
             Column(
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(16.dp),
+                    .padding(16.dp)
+                    .verticalScroll(scrollState),
                 verticalArrangement = Arrangement.Top,
                 horizontalAlignment = Alignment.Start
             ) {
