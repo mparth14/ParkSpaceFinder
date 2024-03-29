fd8eab107b6235952689c00322f9e4b68b6d979b
Parth Pinakin Modi <pr571545@dal.ca>
2024-03-29 17:52:47 -0300

    added animation and sound for user feedback

---
+++ app/src/main/java/com/parkspace/finder/ui/payment/KonfettiViewModel.kt
@@ -0,0 +1,22 @@
+package com.parkspace.finder.ui.payment
+
+import androidx.compose.runtime.Composable
+import nl.dionsegijn.konfetti.core.Party
+import nl.dionsegijn.konfetti.core.Position
+import nl.dionsegijn.konfetti.core.emitter.Emitter
+import java.util.concurrent.TimeUnit
+
+@Composable
+fun KonfettiViewModel(): List<Party> {
+    return listOf(
+        Party(
+            speed = 0f,
+            maxSpeed = 30f,
+            damping = 0.9f,
+            spread = 360,
+            colors = listOf(0xfce18a, 0xff726d, 0xf4306d, 0xb48def),
+            emitter = Emitter(duration = 100, TimeUnit.MILLISECONDS).max(100),
+            position = Position.Relative(0.5, 0.3)
+        )
+    )
+}
